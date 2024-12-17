36f94594bf75967696648efafcb08c7f072e9af5
Chahine-tech <chahine.benlahcen@gmail.com>
2024-12-17 10:41:23 +0100

    refactor(PersonInfo): update data types to use uint256 for age and timestamps

--- day02/main.sol
+++ day02/main.sol
@@ -5,49 +5,58 @@ pragma solidity >=0.7.0 <0.9.0;
 contract PersonInfo {
     struct Person {
         string name;
-        uint age;
+        uint256 age;
     }
 
     Person public person;
-    uint[] public timestamp;
+    uint256[] public timestamp;
     Person[] public people;
-    uint public personCount;
+    uint256 public personCount;
 
-    constructor(string memory _name, uint _age) {
+    constructor(string memory _name, uint256 _age) {
         person = Person(_name, _age);
         addPerson(_name, _age);
     }
 
-    function updatePerson(string memory _name, uint _age) public {
+    function updatePerson(string memory _name, uint256 _age) public {
         person.name = _name;
         person.age = _age;
         timestamp.push(block.timestamp);
-    } 
+    }
 
-    function get() public view returns (string memory, uint ) {
+    function get() public view returns (string memory, uint256) {
         return (person.name, person.age);
     }
 
-    function getTimestamp() public view returns(uint[] memory) {
+    function getOne(uint256 personIndex) public view returns (Person memory) {
+        require(personIndex < personCount, "Index out of bounds");
+        return people[personIndex];
+    }
+
+    function getAll() public view returns (Person[] memory) {
+        return people;
+    }
+
+    function getTimestamp() public view returns (uint256[] memory) {
         return timestamp;
     }
 
-    function cal(uint a, uint b) public pure returns(uint) {
+    function cal(uint256 a, uint256 b) public pure returns (uint256) {
         return a + b;
     }
 
-    function addPerson(string memory _name, uint _age) public {
+    function addPerson(string memory _name, uint256 _age) public {
         people.push(Person(_name, _age));
         personCount++;
     }
-    
+
     function removeLastPerson() public {
         require(personCount > 0, "No person to remove");
         people.pop();
         personCount--;
     }
-    
-    function countPeople() public view returns (uint) {
+
+    function countPeople() public view returns (uint256) {
         return personCount;
     }
 }
