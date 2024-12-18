dd5eeb5c37a80ad7e139ce0e54e20430f08a50d7
Chahine-tech <chahine.benlahcen@gmail.com>
2024-12-18 11:56:34 +0100

    feat(task-management): add task management functions for resetting, deleting, and completing all tasks

--- day03/exo/TaskManagement.sol
+++ day03/exo/TaskManagement.sol
@@ -15,6 +15,10 @@ contract TaskManagement {
 
     event TaskAdded(uint256 id, string description);
     event TaskCompleted(uint256 id);
+    event TaskReset(uint256 id);
+    event TaskDeleted(uint256 id);
+    event ProjectOwnerChanged(address newOwner);
+    event AllTasksCompleted();
 
     constructor() {
         owner = msg.sender;
@@ -33,10 +37,49 @@ contract TaskManagement {
 
     function completeTask(uint256 _id) public onlyOwner {
         require(_id < numberOfTasks, "Task does not exist");
-        Task storage task = tasks[_id];
-        require(!task.completed, "Task is already completed");
-        task.completed = true;
+        require(!tasks[_id].completed, "Task is already completed");
+
+        tasks[_id].completed = true;
         emit TaskCompleted(_id);
+    }
+
+    function resetTask(uint256 _id) public onlyOwner {
+        require(_id < numberOfTasks, "Task does not exist");
+        require(tasks[_id].completed, "Task is not completed");
+
+        tasks[_id].completed = false;
+        emit TaskReset(_id);
+    }
+
+    function deleteTask(uint256 _id) public onlyOwner {
+        require(_id < numberOfTasks, "Task does not exist");
+
+        tasks[_id] = tasks[numberOfTasks - 1];
+        delete tasks[numberOfTasks - 1];
+        numberOfTasks--;
+
+        emit TaskDeleted(_id);
+    }
+
+    function changeProjectOwner(address newOwner) public onlyOwner {
+        require(newOwner != address(0), "New owner cannot be the zero address");
+        owner = newOwner;
+        emit ProjectOwnerChanged(newOwner);
+    }
+
+    function completeAllTasks() public onlyOwner {
+        for (uint256 i = 0; i < numberOfTasks; i++) {
+            tasks[i].completed = true;
+        }
+        emit AllTasksCompleted();
+    }
+
+    function listAllDescriptions() public view returns (string[] memory) {
+        string[] memory descriptions = new string[](numberOfTasks);
+        for (uint256 i = 0; i < numberOfTasks; i++) {
+            descriptions[i] = tasks[i].description;
+        }
+        return descriptions;
     }
 
     function getTotalTasks() public view returns (uint256) {
